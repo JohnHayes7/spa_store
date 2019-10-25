c0ac300eb9adfff4d2f4900f676ccaef8a5bb019
John Hayes <JohnnyHayes7@gmail.com>
2019-10-25 15:27:42 -0400

    building out inital fetch request

--- spa_store_frontend/script.js
+++ spa_store_frontend/script.js
@@ -1,2 +1,15 @@
 let div = document.getElementById('main-div')
 div.innerText = "I'm Over Here Now"
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    fetch('http://localhost:3000/stores/2').then(function(response){
+        return response.json();
+        }).then(object => console.log(object));
+} )
+
+
+class Store{
+    constructor(name){
+        this.name = name
+    }
+}
