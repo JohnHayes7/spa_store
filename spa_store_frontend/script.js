29ac0d0335df7ad91e0a8a825f8a0e2d1c24bd4f
John Hayes <JohnnyHayes7@gmail.com>
2019-10-25 17:11:56 -0400

    building out js Store object

--- spa_store_frontend/script.js
+++ spa_store_frontend/script.js
@@ -4,8 +4,20 @@ div.innerText = "I'm Over Here Now"
 document.addEventListener('DOMContentLoaded', (event) => {
     fetch('http://localhost:3000/stores/2').then(function(response){
         return response.json();
-        }).then(object => console.log(object));
+        }).then(function(json){
+            let name = json["data"]["attributes"]["name"]
+            
+            store = createStore(name)
+            let h1 = document.createElement('h1');
+            h1.innerText = store.name
+            document.body.appendChild(h1);
+            
+        });
 } )
+
+function createStore(name){
+    return new Store (name)
+}
 
 
 class Store{
