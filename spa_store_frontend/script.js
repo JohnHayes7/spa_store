5a737be9c0f060d63c023a8986858c769264236d
John Hayes <JohnnyHayes7@gmail.com>
2019-10-26 13:27:06 -0400

    updated js and css for header

--- spa_store_frontend/script.js
+++ spa_store_frontend/script.js
@@ -1,5 +1,3 @@
-let div = document.getElementById('main-div')
-div.innerText = "I'm Over Here Now"
 
 document.addEventListener('DOMContentLoaded', (event) => {
     fetch('http://localhost:3000/stores/2').then(function(response){
@@ -7,10 +5,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }).then(function(json){
             let name = json["data"]["attributes"]["name"]
             
-            store = createStore(name)
+            store = createStore(name);
+            let header = document.getElementById("main-head");
             let h1 = document.createElement('h1');
-            h1.innerText = store.name
-            document.body.appendChild(h1);
+            h1.innerText = "Welcome to " + store.name;
+            header.appendChild(h1);
+           
             
         });
 } )
