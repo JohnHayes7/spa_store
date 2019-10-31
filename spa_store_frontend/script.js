641483e5d6b6d1c05e0bc83086cefea86c48ce8d
John Hayes <JohnnyHayes7@gmail.com>
2019-10-31 10:20:29 -0400

    Added vendors display to page

--- spa_store_frontend/script.js
+++ spa_store_frontend/script.js
@@ -6,19 +6,27 @@ document.addEventListener('DOMContentLoaded', (event) => {
             let name = json["data"]["attributes"]["name"]
             let vendors = json["included"].filter(event => event["type"] === "vendor")
             store = createStore(name, vendors);
-            debugger
             let header = document.getElementById("main-head");
             let h1 = document.createElement('h1');
             h1.innerText = "Welcome to " + store.name;
             header.appendChild(h1);
+            addVendors(store.vendors)
            
             
         });
 } )
 
-function addVendors(vendors){
-    let topVs = document.getElementById('top-vendors')
-    topVs.innerText = vendors
+
+
+function addVendors(vendorsAry){
+    let topVs = document.getElementById('top-vendors-list')
+    vendorsAry.forEach(vendor => {
+        let li = document.createElement('li');
+        li.innerText = vendor.attributes.name
+        topVs.appendChild(li)
+    });
+   
+    
 }
 
 function createStore(name, vendors){
