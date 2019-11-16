fa114fc93db0de1f29946cad7c315bc4bfe1fe98
John Hayes <JohnnyHayes7@gmail.com>
2019-11-16 11:27:42 -0500

    reorganized vendor functions to own js

--- spa_store_frontend/js/script.js
+++ spa_store_frontend/js/script.js
@@ -17,8 +17,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
             addVendors(store.vendors)
             addProducts(store.products)
             addCategories(store.categories)
-            
-            
         });
 
 
@@ -60,17 +58,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 
 
-function addVendors(vendorsAry){
-    let topVs = document.getElementById('top-vendors-list') 
-    vendorsAry.forEach(v => {
-        let vId = v.id 
-        let vName = v.attributes.name
-        let vTagline = v.attributes.tagline
 
-        let vendor = createVendor(vId, vName, vTagline)
-        createVendorPreviewElements(vendor, topVs)
-    });
-}
 
 function addCategories(categoriesAry){
     let popCatsList = document.getElementById('pop-cats-list')
@@ -85,29 +73,7 @@ function addCategories(categoriesAry){
 }
 
 
-function vendorProductsList(products){
-    let browser = document.getElementById('browse');
-    let detailsDiv = document.createElement("div");
-    let divAttr = document.createAttribute("id");
-    divAttr.value = "details";
-    detailsDiv.setAttributeNode(divAttr);
-    let ul = document.createElement('ul');
 
-    // function parseAndCreateProducts???
-    Array.from(products).forEach(p => {
-        let pId = p.id;
-        let pName = p.attributes.name;
-        let pDescription = p.attributes.description;
-        let pPrice = p.attributes.price;
-        let pImgPath = p.attributes.image_path
-
-        let product = createProduct(pId, pName, pDescription, pPrice, pImgPath)
-
-        createProductsPreviewElements(product, ul)
-        detailsDiv.appendChild(ul)
-        browser.appendChild(detailsDiv);
-    })
-}
 
 function categoryProductsList(products){
     let browser = document.getElementById('browse');
@@ -192,51 +158,7 @@ function createCatPreviewElements(category, parent){
 
 
 // VENDOR HELPER FUNCTIONS
-function createVendorPreviewElements(vendor, parent){
-    let li = document.createElement('li');
-    let link = document.createElement('a')
-    let href = document.createAttribute('href')
-    let dataID = document.createAttribute('data-id')
-    let c = document.createAttribute('class')
-    dataID.value = vendor.id
-    c.value = "vend-select"
-    link.innerText = vendor.name + " - "  + vendor.tagline
-    link.setAttributeNode(dataID)
-    link.setAttributeNode(href)
-    link.setAttributeNode(c)
-    li.appendChild(link)
-    makeVendorClickable(vendor, li)
-    parent.appendChild(li)
-}
 
-function makeVendorClickable(object, element){
-    element.addEventListener("click", function(e){
-         e.preventDefault()
-         fetchVendors(object)       
-    })
-}
-
-function fetchVendors(object){
-    fetch(`http://localhost:3000/vendors/${object.id}`).then(function(response){
-        return response.json()
-    }).then(function(json){
-        let products = json["included"].filter(event => event["type"] === "product")
-            
-        object.products = products
-        let browser = document.getElementById("browse")
-        let subHead = document.createElement('div')
-        let subAtt = document.createAttribute("id")
-            
-        clearWindow(browser)
-
-        subAtt.value = "subAtt";
-        subHead.setAttributeNode(subAtt);
-        subHead.innerText = `All Products from ${object.name} \n ${object.tagline} ` 
-        browser.appendChild(subHead)
-        vendorProductsList(object.products)
-
-    })   
-}
 
 function makeCategoryClickable(element, category){
     element.addEventListener('click', function(e){
